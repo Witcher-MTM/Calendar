d39d5892bf6ba9aa04391a2165f3f4b219e3a555
Witcher-MTM <86706397+Prikhod322@users.noreply.github.com>
2021-07-08 11:00:53 +0300

    Add DLL

---
+++ DLL/DLL/Note.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace DLL
+{
+    public class Note
+    {
+        public string NoteName { get; set; }
+        public string Text { get; set; }
+        public DateTime CreationData { get; private set; }
+
+        public Note()
+        {
+            NoteName = string.Empty;
+            Text = string.Empty;
+            CreationData = new DateTime();
+        }
+
+        public Note(string noteName, string text, DateTime creationData)
+        {
+            Text = text;
+            NoteName = noteName;
+            CreationData = creationData;
+        }
+    }
+}

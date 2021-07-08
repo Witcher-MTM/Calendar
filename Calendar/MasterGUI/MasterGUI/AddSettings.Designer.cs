f6193b525262cc67cc372944485aa50620353848
Witcher-MTM <86706414+Witcher-MTM@users.noreply.github.com>
2021-07-08 19:52:22 +0300

    All folders

---
+++ Calendar/MasterGUI/MasterGUI/AddSettings.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace MasterGUI
+{
+    partial class AddSettings
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.accBtn = new System.Windows.Forms.Button();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.SuspendLayout();
+            // 
+            // accBtn
+            // 
+            this.accBtn.Location = new System.Drawing.Point(154, 1);
+            this.accBtn.Name = "accBtn";
+            this.accBtn.Size = new System.Drawing.Size(75, 23);
+            this.accBtn.TabIndex = 0;
+            this.accBtn.Text = "Confirm";
+            this.accBtn.UseVisualStyleBackColor = true;
+            this.accBtn.Click += new System.EventHandler(this.accBtn_Click);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(12, 211);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 1;
+            this.dateTimePicker1.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+            // 
+            // AddSettings
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(231, 243);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.accBtn);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.Name = "AddSettings";
+            this.Text = "AddSettings";
+            this.Load += new System.EventHandler(this.AddSettings_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button accBtn;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+    }
+}
